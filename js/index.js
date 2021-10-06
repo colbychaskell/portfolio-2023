9efd1ed771f45e158002158ce6f69ef12da0f722
Colby Haskell <cchaskel@usc.edu>
2021-10-06 10:27:04 -0700

    Links on Projects

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-$(function() {
+$(document).ready(function() {
     $(".fade1").fadeIn(500)
     $(".fade2").fadeIn(1000)
-})
+
+    $(".project-display").hover(function() {
+        $("img", this).animate({opacity: 0.3}, 200);
+        $("p", this).fadeIn()
+    }, function() {
+        $("img", this).animate({opacity: 1}, 200);   
+        $("p", this).fadeOut()
+    });
+});
