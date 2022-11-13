ccf156a6affbf2706bc7a23697f5bf01278dd80d
Colby Haskell <cchaskel@usc.edu>
2022-11-13 15:22:42 -0800

    Fix mobile site 🛠

--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -4,11 +4,9 @@ import './SocialLink.css';
 
 export const SocialLink = (props) => {
     return (
-        <>
-            <a className="social-link" href={props.link} target="_blank" rel="noopener noreferrer">
-                <img src={props.icon} alt={props.alt} />
-            </a>
-        </>
+        <a className="social-link" href={props.link} target="_blank" rel="noopener noreferrer">
+            <img src={props.icon} alt={props.alt} />
+        </a>
     );
 }
 
