8d109f2b9cb3b124c29a42719528b7572b900be3
Colby Haskell <cchaskel@usc.edu>
2022-11-13 10:10:00 -0800

    Fix Responsiveness 🛠

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,7 +8,8 @@ export const Profile = (props) => {
             <img src={ProfileImage} alt="Profile" />
             <div className="bio">
                 <h2>Front-End Software Engineer</h2>
-                <p>I am focused on creating elegant and powerful Front-End experiences. My favorite technologies are currently ReactJS and SwiftUI. My other skills include C++, Python, and Machine Learning. </p>
+                <p>I am a software engineer and a senior studying CE/CS at USC. I am dedicated to building elegant and powerful front-end user experiences. My favorite technologies at the moment are ReactJS and SwiftUI. My other skills include C++, Python, and Machine Learning. </p>
+                <p>Feel free to reach out by email or connect with me on Linkedin!</p>
             </div>
         </div>
     );
