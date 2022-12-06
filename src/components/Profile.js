c0c8590a1b5cf85add32b20c81828522c9941c86
Colby Haskell <cchaskel@usc.edu>
2022-12-06 10:16:14 -0800

    Update Profile Title and Description

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,8 +7,8 @@ export const Profile = (props) => {
         <div className="profile">
             <img src={ProfileImage} alt="Profile" />
             <div className="bio">
-                <h2>Front-End Software Engineer</h2>
-                <p>I am a software engineer and a senior studying CE/CS at USC. I am working to build elegant and powerful front-end user experiences. My favorite technologies at the moment are ReactJS and SwiftUI. My other skills include C++, Python, and Machine Learning. </p>
+                <h2>Full Stack Software Engineer</h2>
+                <p>I am a software engineer and a senior studying CE/CS at USC. I have experience with full stack web development, embedded systems, and software for IoT devices. My favorite technologies at the moment are ReactJS and SwiftUI.  </p>
                 <p>Feel free to reach out by email or connect with me on Linkedin!</p>
             </div>
         </div>
