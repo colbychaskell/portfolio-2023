fa56722e8aa00d6f65c63c14c7a77198d0d1342c
Colby Haskell <cchaskel@usc.edu>
2024-01-13 20:33:17 -0500

    Update styles using tailwind classes

--- src/containers/ProjectsFeed.tsx
+++ src/containers/ProjectsFeed.tsx
@@ -1,31 +1,33 @@
-import React from 'react';
 import { ProjectPreview } from '../components/ProjectPreview';
 import { useState, useEffect } from 'react';
 import { getProjects } from '../services/Github';
-import './ProjectsFeed.css';
 
 export const ProjectsFeed = (props) => {
     // Useeffect call to get projects from github
-    const [ projects, setProjects ] = useState([{name: "Loading...", description: "Loading...", link: "#"}]);
+    const [projects, setProjects] = useState([{ name: "Loading...", description: "Loading...", link: "#" }]);
 
-    useEffect( () => {
+    useEffect(() => {
         getProjects().then((data) => {
             setProjects(data);
-    })}, []);
+        })
+    }, []);
 
-    return (  
-        <div className="projects-feed">
-            { projects.map((project, index) => {
-                const description = project.description == null ? "A very exciting project!" : project.description;
-                return  <ProjectPreview key={index} 
-                                        name={project.name} 
-                                        description={description} 
-                                        link={project.link} 
-                                        stars={project.stars} 
-                                        forks={project.forks} 
-                                        issues={project.issues}
-                                        tags={project.topics} />;
-            }) }
-        </div>
+    const projectsLoaded = projects.map((project, index) => {
+        return <ProjectPreview key={index}
+            name={project.name}
+            description={project.description || "A very exciting project!"}
+            link={project.link}
+            stars={project.stars}
+            forks={project.forks}
+            issues={project.issues}
+            tags={project.topics} />;
+    });
+
+    return (
+        <>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center py-4">
+                {props.isExpanded ? projectsLoaded : projectsLoaded.slice(0, 6)}
+            </div>
+        </>
     );
 }
--- src/containers/TitledSection.tsx
+++ src/containers/TitledSection.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import './TitledSection.css';
+import { useState } from 'react';
+import { ProjectsFeed } from './ProjectsFeed';
+import { ChevronDoubleDownIcon } from '@heroicons/react/24/outline';
+import { ChevronDoubleUpIcon } from '@heroicons/react/24/outline';
 
 export const TitledSection = (props) => {
+    const [isExpanded, setExpanded] = useState(false);
+
+    const seeMoreButton = (
+        <div className="flex flex-col py-6 hover:cursor-pointer hover:text-blue-800 transition ease-in-out"
+            onClick={() => {setExpanded(true)}}>
+            <h2 className='text-lg font-semibold'>
+                See More
+            </h2>
+            <ChevronDoubleDownIcon className="h-10 font-semibold" />
+        </div>);
+
+    const seeLessButton = (
+        <div className="flex flex-col py-6 hover:cursor-pointer hover:text-blue-800 transition ease-in-out"
+            onClick={() => {setExpanded(false)}}>
+            <h2 className='text-lg font-semibold'>
+                See Less
+            </h2>
+            <ChevronDoubleUpIcon className="h-10 font-semibold" />
+        </div>);
+
     return (
-        <section className="titled-section" style={{"background-color": props.backgroundColor}}>
-            <div className="section-header">
-                <div className="section-title">
-                    <h1>{ props.title }</h1>
-                    <h4>{ props.subtitle }</h4>
-                </div>
+        <section className="flex flex-col items-center overflow-hidden w-full bg-sky-50">
+            <div className="flex flex-col items-start p-10 w-full">
+                <h1 className="text-xl font-semibold text-left">{props.title}</h1>
+                <h4 className="text-left">{props.subtitle}</h4>
             </div>
-            <div className="section-content">
-                { props.children }
-            </div>
+            <ProjectsFeed isExpanded={isExpanded} />
+            {isExpanded ? seeLessButton : seeMoreButton}
         </section>
     );
 }
